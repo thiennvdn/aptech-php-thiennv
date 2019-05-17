60f77dd4184f3ef161555c44c2aec46949af364f
thiennv <vietthien211@gmail.com>
2019-05-17 09:40:26 +0700

    update

---
+++ html/08-16052019/script.js
@@ -0,0 +1,12 @@
+// var ten = 'Nguyen Viet Thien';
+// var lop = 'PHP-18';
+// var thongBao = ten +' '+ lop;
+
+// console.log(thongBao);
+
+function hienThiKetQua() {
+    document.getElementById('demo').innerHTML = 'Xin Chao';
+}
+function hienThiThayDoi() {
+    document.getElementById('a').style.fontSize = "100px";
+}
