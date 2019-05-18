eec81a11bcb00a648088b1911eaed81f6780c59b
thiennv <vietthien211@gmail.com>
2019-05-18 18:47:25 +0700

    update08

--- html/08-16052019/script.js
+++ html/08-16052019/script.js
@@ -4,9 +4,20 @@
 
 // console.log(thongBao);
 
-function hienThiKetQua() {
-    document.getElementById('demo').innerHTML = 'Xin Chao';
-}
-function hienThiThayDoi() {
-    document.getElementById('a').style.fontSize = "100px";
+// function hienThiKetQua() {
+//     document.getElementById('demo').innerHTML = 'Xin Chao';
+// }
+// function hienThiThayDoi() {
+//     document.getElementById('a').style.fontSize = "100px";
+// }
+var imageElement = document.getElementById('image');
+
+console.log(imageElement.src);
+
+function bongDen() {
+    if (imageElement.src == 'http://127.0.0.1:5500/pic_bulboff.gif') {
+        imageElement.src = 'pic_bulbon.gif';
+    } else {
+        imageElement.src = 'pic_bulboff.gif';
+    }
 }
