039761db9d174160e12aa2825c996a8b2f1ef57d
thiennv <vietthien211@gmail.com>
2019-05-18 21:11:46 +0700

    update

--- html/09-18052019/carousel/script.js
+++ html/09-18052019/carousel/script.js
@@ -17,10 +17,22 @@ function chuyenSlide() {
             document.getElementById('hinh-3').style.display = 'none';
         }
     }
+    setTimeout(chuyenSlide,2000);
 }
 
-function autoSlide() {
-    setInterval(function () {
-        chuyenSlide();
-    }, 3000);
+setTimeout(chuyenSlide,2000);
+
+// function autoSlide() {
+//     setInterval(function () {
+//         chuyenSlide();
+//     }, 3000);
+// }
+
+// setInterval(chuyenSlide, 2000);
+
+var myVar = setInterval(myClock, 1000);
+function myClock() {
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    document.getElementById('demo').innerHTML = t;
 }
