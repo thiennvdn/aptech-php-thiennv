dc8dd3aeb59f39a8ce64a2c2298c374432907033
thiennv <vietthien211@gmail.com>
2019-05-27 11:18:58 +0700

    update-table-jquery

--- html/12-25052019/jQuery/table/script.js
+++ html/12-25052019/jQuery/table/script.js
@@ -1,5 +1,35 @@
 $('button').click(function() {
-    $.get('duong dan', function (data, status) {
-        console.log(data, status);
+    $.get('data.json', function (data, status) {
+        console.log(data.users);
+
+        //tao head table: ID, Name, Email
+        var arrayHead = ['ID','Name','Email'];
+        var head = ''; 
+        for (var i = 0; i < arrayHead.length; i ++) {
+          head += '<th>'+ arrayHead[i] + '</th>';
+        }
+        //tao body table
+        var body = '';
+        for (var i = 0; i < data.users.length; i++) {
+            var dataBody = '';
+            var array = [data.users[i].id, data.users[i].name, data.users[i].email];
+            var a = '';
+            for (var j = 0; j < array.length; j++) {
+                a += '<td>'+ array[j] + '</td>';
+                console.log(a);
+            }
+            dataBody += '<tr>'+ a + '</tr>';
+            console.log(dataBody);
+            body += dataBody; 
+        }   
+        console.log(body); 
+        
+        //xuat bang ra man hinh khi click button
+        $('button').after('<table><thead><tr>' + 
+        head
+        +'</tr></thead>'+
+        '<tbody>'+
+        body
+        +'</tbody></table>')
     })
 })
