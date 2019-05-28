5f700bd962f2f85fa8a1b4180df49a5616f37029
thiennv <vietthien211@gmail.com>
2019-05-28 09:14:37 +0700

    update

--- html/12-25052019/jQuery/slide/script.js
+++ html/12-25052019/jQuery/slide/script.js
@@ -16,21 +16,37 @@
 
 //cach 2:
 
+// $('#hinh-2').hide();
+// $('#hinh-3').hide();
+
+// $(document).ready(function() {
+//     $('#hinh-1').click(function() {
+//         $('#hinh-1').hide();
+//         $('#hinh-2').show();
+//     })
+//     $('#hinh-2').click(function() {
+//         $('#hinh-2').hide();
+//         $('#hinh-3').show();
+//     })
+//     $('#hinh-3').click(function() {
+//         $('#hinh-3').hide();
+//         $('#hinh-1').show();
+//     })
+// })
+
+//cach 3
+
 $('#hinh-2').hide();
 $('#hinh-3').hide();
 
 $(document).ready(function() {
-    $('#hinh-1').click(function() {
-        $('#hinh-1').hide();
-        $('#hinh-2').show();
-    })
-    $('#hinh-2').click(function() {
-        $('#hinh-2').hide();
-        $('#hinh-3').show();
-    })
-    $('#hinh-3').click(function() {
-        $('#hinh-3').hide();
-        $('#hinh-1').show();
-    })
+    function slide (a,b) {
+        $(a).click(function() {
+            $(a).hide();
+            $(b).show();
+        })
+    }
+    slide('#hinh-1','#hinh-2');
+    slide('#hinh-2','#hinh-3');
+    slide('#hinh-3','#hinh-1');
 })
-
