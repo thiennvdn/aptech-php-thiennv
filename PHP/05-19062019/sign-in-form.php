dd647b3aa1641f13396c916342f5a91a5d9a2d01
thiennv <vietthien211@gmail.com>
2019-06-19 23:51:31 +0700

    update

--- PHP/05-19062019/sign-in-form.php
+++ PHP/05-19062019/sign-in-form.php
@@ -7,26 +7,47 @@
     <title>SIGN IN</title>
 </head>
 <body>
-    <!-- <h2>SIGN IN:</h2>
-    <form action="welcome.php" method="POST">
-        Name: <input type="text" name="name" id=""><br><br>
+    <h2>SIGN IN:</h2>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
         E-mail: <input type="email" name="email" id=""><br><br>
         Password: <input type="password" name="password" id=""><br><br>
         <input type="submit" value="submit"><br><br>
-    </form> -->
+    </form>
     <?php
         require "php-connect-mysql.php";
         $conn = connectDB();
-        $select = "SELECT id, name, password FROM ap_18_4.users";
+        $select = "SELECT * FROM ap_18_4.users";
         $result = mysqli_query($conn, $select);
-        if (mysqli_num_rows($result) > 0) {
-            // output data of each row
-            while($row = mysqli_fetch_assoc($result)) {
-                echo "Name: " . $row["name"]. " - Email: " . $row["email"]. " - Password: " . $row["password"]. "<br>";
-            }
-        } else {
-            echo "0 results";
-        }
+        $arr = mysqli_fetch_all($result,MYSQLI_ASSOC);
+        // var_dump ($result);
+        // $a = [];
+        // var_dump("aa");
+        // var_dump($_POST);
+        // echo "<br>";
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $a = count($arr);
+        var_dump($arr);
+        for ($i=0; $i < $a; $i++) { 
+            echo $arr[$i]["email"] . "<br>";
+        } 
+        // if ($email == $arr[$i]["email"] && $password == $arr[$i]["password"]) {
+        //     echo "Dang nhap thanh cong";
+        // } else {
+        //     echo "Dang nhap that bai";
+        // }
+        
+       
+    
+        
+        // if (mysqli_num_rows($result) > 0) {
+        //     // output data of each row
+        //     while($row = mysqli_fetch_assoc($result)) {
+        //         echo "Name: " . $row["name"]. " - Email: " . $row["email"]. " - Password: " . $row["password"]. "<br>";
+        //     }
+        // } else {
+        //     echo "0 results";
+        // }
         
         mysqli_close($conn);
     ?>
